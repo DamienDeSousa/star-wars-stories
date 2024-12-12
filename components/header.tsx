3eca139f72befce71a2fe8dddb09b4a529a6eb65
DamienDeSousa <desousadamien30@gmail.com>
2024-12-12 21:46:27 +0100

    add shadow for better title reading

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
 
   return (
     <>
-      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 sm:mb-6 text-yellow-400">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 sm:mb-6 text-secondary drop-shadow-xl">
         {t("title")}
       </h1>
       <p className="mb-4 sm:mb-6 text-base sm:text-lg md:text-xl">
